3f1cc33241974235fa9fc2ebbdd0666b79637eae
Christopher J. Scharer <ChrisScharer1416@msn.com>
2018-02-11 16:10:48 -0600

    Name and formatting.

--- src/test/java/com/cjs/qa/serenity/cucumberjvm/DefinitionTestSuite.java
+++ src/test/java/com/cjs/qa/serenity/cucumberjvm/DefinitionTestSuite.java
@@ -1,8 +1,9 @@
 package com.cjs.qa.serenity.cucumberjvm;
 
+import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
--- src/test/java/com/cjs/qa/serenity/cucumberjvm/steps/DefinitionSteps.java
+++ src/test/java/com/cjs/qa/serenity/cucumberjvm/steps/DefinitionSteps.java
@@ -1,30 +1,30 @@
 package com.cjs.qa.serenity.cucumberjvm.steps;
 
-import net.thucydides.core.annotations.Steps;
+import com.cjs.qa.serenity.cucumberjvm.steps.serenity.EndUserSteps;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import com.cjs.qa.serenity.cucumberjvm.steps.serenity.EndUserSteps;
+import net.thucydides.core.annotations.Steps;
 
 public class DefinitionSteps {
 
     @Steps
-    EndUserSteps anna;
+    EndUserSteps endUserSteps;
 
     @Given("the user is on the Wikionary home page")
     public void givenTheUserIsOnTheWikionaryHomePage() {
-        anna.is_the_home_page();
+        endUserSteps.is_the_home_page();
     }
 
     @When("the user looks up the definition of the word '(.*)'")
     public void whenTheUserLooksUpTheDefinitionOf(String word) {
-        anna.looks_for(word);
+        endUserSteps.looks_for(word);
     }
 
     @Then("they should see the definition '(.*)'")
     public void thenTheyShouldSeeADefinitionContainingTheWords(String definition) {
-        anna.should_see_definition(definition);
+        endUserSteps.should_see_definition(definition);
     }
 
 }
